8bdc32588c4fec47154ac231ced4a8a87d248b52
Anubhav Mittal <anubhav11march@gmail.com>
2019-10-19 22:45:25 +0530

    :green_heart: drugInfo end

--- Application/app/src/main/java/com/example/hackabit/MAct.java
+++ Application/app/src/main/java/com/example/hackabit/MAct.java
@@ -24,9 +24,11 @@ public class MAct extends AppCompatActivity {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     fragment = new Scan();
+                    getSupportActionBar().setTitle("Drug Info");
                     break;
                 case R.id.navigation_dashboard:
                     fragment = new Chat();
+                    getSupportActionBar().setTitle("Reminders");
                     break;
             }
             if(fragment != null){
@@ -45,6 +47,7 @@ public class MAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m);
+        getSupportActionBar().setTitle("Drug Info");
         BottomNavigationView navView = findViewById(R.id.nav_view);
         Fragment frag = new Scan();
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
