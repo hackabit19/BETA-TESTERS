481f674f7cdd0c3ee51f22c19b1655584f927b5c
Anubhav Mittal <anubhav11march@gmail.com>
2019-10-19 11:33:01 +0530

    :fire: cleaned code

--- Application/app/src/main/java/com/example/hackabit/Scan.java
+++ Application/app/src/main/java/com/example/hackabit/Scan.java
@@ -30,26 +30,10 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link Scan.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link Scan#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Scan extends Fragment {
 
     Button scan;
     int BARCODE_READER  =1;
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -57,31 +41,10 @@ public class Scan extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Scan.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Scan newInstance(String param1, String param2) {
-        Scan fragment = new Scan();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -90,14 +53,11 @@ public class Scan extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
         scan = (Button) view.findViewById(R.id.scan);
-        scan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+
                 Intent intent = new Intent(getContext(), BarcodeCapture.class);
                 startActivityForResult(intent, BARCODE_READER);
 //                startActivity(new Intent(MainActivity.this, MAct.class));
-            }
-        });
+
         return view;
     }
 
@@ -105,20 +65,22 @@ public class Scan extends Fragment {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == BARCODE_READER){
             if(resultCode == CommonStatusCodes.SUCCESS){
-                if(data!=null){
-                    Barcode barcode = data.getParcelableExtra(BarcodeCapture.BarcodeObject);
-                    Point[] p = barcode.cornerPoints;
-                    Toast.makeText(getContext(), barcode.displayValue + "success", Toast.LENGTH_SHORT).show();
-//                    amount.setText("Amount Last Received: " + barcode.displayValue);
-                    try{
-                        getApi();
-                    }catch (Exception e){
-                        e.printStackTrace();
+                try {
+                    if (data != null) {
+                        Barcode barcode = data.getParcelableExtra(BarcodeCapture.BarcodeObject);
+                        Point[] p = barcode.cornerPoints;
+                        Toast.makeText(getContext(), barcode.displayValue + "success", Toast.LENGTH_SHORT).show();
+                        try {
+                            getApi();
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Toast.makeText(getContext(), "Failed to capture", Toast.LENGTH_SHORT).show();
                     }
-
-
-                }else {
-                    Toast.makeText(getContext(), "Fail", Toast.LENGTH_SHORT).show();
+                }catch (Exception e){
+                    startActivity(new Intent(getContext(), MAct.class));
+                    Toast.makeText(getContext(), "An error occured, please try again", Toast.LENGTH_LONG).show();
                 }
             }else {
                 Log.v("AAA", CommonStatusCodes.getStatusCodeString(resultCode));
@@ -157,7 +119,6 @@ public class Scan extends Fragment {
         });
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
@@ -170,17 +131,7 @@ public class Scan extends Fragment {
         super.onDetach();
         mListener = null;
     }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
